d2769f85675a16ab0b5cf5c81fee9cb6b421f7ae
Peter Law <PeterJCLaw@gmail.com>
2012-04-14 20:19:18 +0100

    Ensure valid json in the 0th match.

--- matchmaker.py
+++ matchmaker.py
@@ -4,7 +4,7 @@ import redis, json
 from random import choice
 
 teams = ['ARG','BWS','BWS2','BGR','BRK','BSG','CLF','CLF2','EMM','GRS','GMR','HRS','HZW','MFG','MUC','PSC','PSC2','QEH','QMC','SEN','SEN2','TTN']
-matches = ["{'time':3600,'teams': ['PSC','MFG','BSG','QMC'], 'delay': 1}"]
+matches = [json.dumps(dict(time = 3600, teams = ["PSC","MFG","BSG","QMC"], delay = 1))]
 
 actor = redis.Redis(host='localhost',port=6379,db=0)
 
